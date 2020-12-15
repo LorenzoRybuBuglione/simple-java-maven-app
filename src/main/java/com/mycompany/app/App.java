c8224ffd64bdd531390a538f88b2387fa3e24fcb
LorenzoRybuBuglione <55191240+LorenzoRybuBuglione@users.noreply.github.com>
2020-12-14 21:41:46 -0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,21 +1,49 @@
 package com.mycompany.app;
+import java.util.Scanner;
 
-/**
- * Hello world!
- */
-public class App
-{
+public class Calc {
 
-    private final String message = "Hello World!";
+    public static int op(int first, char operator, int second) {
 
-    public App() {}
+        int result;
 
+        switch (op) {
+        case '+':
+            result = first + second;
+            return result;
+
+        case '-':
+            result = first - second;
+            return result;
+
+        case '*':
+            result = first * second;
+            return result;
+
+        case '/':
+            result = first / second;
+            return result;
+
+        default:
+            System.out.printf("Error! operator is not correct");
+            return 0;
+        }
+    }
+
+    
     public static void main(String[] args) {
-        System.out.println(new App().getMessage());
-    }
+        int sum, minus, mult, div;
 
-    private final String getMessage() {
-        return message;
-    }
+        sum = Calc.op(7,'+',2);
+        System.out.printf("\n7+3 =%d", sum);
 
+        minus = Calc.op(4,'-',2);
+        System.out.printf("\n4-2=%d", sub);
+
+        mult = Calc.op(2,'*',3);
+        System.out.printf("\n2*3=%d", mult);
+
+        div = Calc.op(6,'/',2);
+        System.out.printf("\n6/2=%d\n", div);
+    }
 }
